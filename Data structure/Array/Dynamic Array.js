06b7221711ca20db338f06d747a6c27eed8fdc6f
ZhimingDeng5 <80497374+ZhimingDeng5@users.noreply.github.com>
2022-07-15 09:36:08 +1000

    Create Dynamic Array.js

---
+++ Data structure/Array/Dynamic Array.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function dynamicArray(n, queries) {
+    let arr = new Array(n)
+    for (let j=0;j<n;j++)
+    {
+        arr[j]=new Array()
+    }
+    let lastAnswer=0
+    let result=[]
+    for (let i in queries){
+        let t= (lastAnswer^queries[i][1])%n;
+        let y= queries[i][2]
+        if (queries[i][0]==1)
+        {
+                arr[t].push(queries[i][2]);
+        }
+        else
+        {
+            lastAnswer=arr[t][y%(arr[t].length)]
+            result.push(lastAnswer)
+        }
+    }
+    return result
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const q = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = dynamicArray(n, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+
